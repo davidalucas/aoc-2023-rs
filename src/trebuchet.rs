4ba0a45fcc1343449a72a6f5dc7147b9c76d5f84
David Lucas <davidalucas92@gmail.com>
2024-01-20 08:40:24 -0500

    Adds parse_digits_enhanced function
    Includes the map for checking the 'number words'

--- src/trebuchet.rs
+++ src/trebuchet.rs
@@ -1,4 +1,5 @@
 use std::{
+    collections::HashMap,
     fs::File,
     io::{BufRead, BufReader},
 };
@@ -39,6 +40,62 @@ pub fn sum_digits(path: &str) -> u64 {
     sum
 }
 
+fn parse_digits_enhanced(data: &str) -> u8 {
+    let num_map: HashMap<&str, u8> = HashMap::from([
+        ("one", 1),
+        ("two", 2),
+        ("three", 3),
+        ("four", 4),
+        ("five", 5),
+        ("six", 6),
+        ("seven", 7),
+        ("eight", 8),
+        ("nine", 9),
+    ]);
+
+    let first_digit = find_first_digit(data, &num_map);
+    let second_digit = find_last_digit(data, &num_map);
+
+    first_digit * 10 + second_digit
+}
+
+fn find_first_digit(data: &str, num_map: &HashMap<&str, u8>) -> u8 {
+    for (i, ch) in data.chars().enumerate() {
+        if ch.is_ascii_digit() {
+            return ch.to_digit(10).unwrap() as u8;
+        }
+        for (key, val) in num_map {
+            if i + key.len() > data.len() {
+                continue;
+            }
+            if &data[i..key.len()] == *key {
+                return *val;
+            }
+        }
+    }
+    0
+}
+
+fn find_last_digit(data: &str, num_map: &HashMap<&str, u8>) -> u8 {
+    for (i, ch) in data.chars().rev().enumerate() {
+        if ch.is_ascii_digit() {
+            return ch.to_digit(10).unwrap() as u8;
+        }
+        for (key, val) in num_map {
+            let curr_pos = data.len() - i - 1;
+            if key.len() > curr_pos {
+                continue;
+            }
+            let start_idx = curr_pos - key.len() + 1;
+            let sub_str = &data[start_idx..(curr_pos + 1)];
+            if sub_str == *key {
+                return *val;
+            }
+        }
+    }
+    0
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -47,5 +104,47 @@ mod tests {
     fn parse_digits_returns_expected_integer() {
         let result = parse_digits("two1nine");
         assert_eq!(result, 11);
+    }
+
+    #[test]
+    fn find_first_digit_finds_first() {
+        let num_map: HashMap<&str, u8> = HashMap::from([
+            ("one", 1),
+            ("two", 2),
+            ("three", 3),
+            ("four", 4),
+            ("five", 5),
+            ("six", 6),
+            ("seven", 7),
+            ("eight", 8),
+            ("nine", 9),
+        ]);
+
+        let result = find_first_digit("two1nine", &num_map);
+        assert_eq!(result, 2);
+    }
+
+    #[test]
+    fn find_last_digit_finds_last() {
+        let num_map: HashMap<&str, u8> = HashMap::from([
+            ("one", 1),
+            ("two", 2),
+            ("three", 3),
+            ("four", 4),
+            ("five", 5),
+            ("six", 6),
+            ("seven", 7),
+            ("eight", 8),
+            ("nine", 9),
+        ]);
+
+        let result = find_last_digit("two1nine", &num_map);
+        assert_eq!(result, 9);
+    }
+
+    #[test]
+    fn parse_digits_enhanced_returns_expected_integer() {
+        let result = parse_digits_enhanced("two1nine");
+        assert_eq!(result, 29);
     }
 }
