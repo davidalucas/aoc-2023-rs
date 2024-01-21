a4900a0b7bd4bd4f947fa219647c6a8a78811441
David Lucas <davidalucas92@gmail.com>
2024-01-20 21:33:07 -0500

    Adds tests for the sum_game_powers function
    Part 2 is now solved

--- tests/cube_game_tests.rs
+++ tests/cube_game_tests.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-use aoc_2023_rs::cube_game::sum_valid_games;
+use aoc_2023_rs::cube_game::*;
 
 #[test]
 fn sum_valid_games_works_for_example_data() {
@@ -14,4 +14,16 @@ fn sum_valid_games_works_for_real_data() {
     let color_limits = HashMap::from([("red", 12), ("green", 13), ("blue", 14)]);
     let result = sum_valid_games("./src/day_2/data.txt", color_limits).unwrap();
     assert_eq!(result, 2727);
+}
+
+#[test]
+fn sum_game_powers_works_for_example_data() {
+    let result = sum_game_powers("./src/day_2/example.txt").unwrap();
+    assert_eq!(result, 2286);
+}
+
+#[test]
+fn sum_game_powers_works_for_real_data() {
+    let result = sum_game_powers("./src/day_2/data.txt").unwrap();
+    assert_eq!(result, 56580);
 }
