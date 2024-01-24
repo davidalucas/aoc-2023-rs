c88e08581eaa64b5a90ded60b81cc10c76404b3d
David Lucas <davidalucas92@gmail.com>
2024-01-24 14:08:23 -0500

    Adds the get_symbols function

--- src/gear_ratios.rs
+++ src/gear_ratios.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 #[derive(Debug, PartialEq)]
 struct PartNumber {
     number: String,
@@ -34,6 +36,18 @@ fn get_part_numbers(data: String) -> Vec<PartNumber> {
     part_numbers
 }
 
+fn get_symbols(data: String) -> HashSet<u32> {
+    let mut sym_positions = HashSet::new();
+
+    for (i, c) in data.chars().enumerate() {
+        if !c.is_digit(10) && c != '.' {
+            sym_positions.insert(i as u32);
+        }
+    }
+
+    sym_positions
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -53,6 +67,27 @@ mod tests {
                     start: 5
                 }
             ]
-        )
+        );
+
+        let actual = get_part_numbers(String::from(".....+.58."));
+        assert_eq!(
+            actual,
+            vec![PartNumber {
+                number: String::from("58"),
+                start: 7
+            }]
+        );
+    }
+
+    #[test]
+    fn get_symbols_works_as_expected() {
+        let actual = get_symbols(String::from("467..114.."));
+        assert_eq!(actual, HashSet::new());
+
+        let actual = get_symbols(String::from("...$.*...."));
+        assert_eq!(actual, HashSet::from([3, 5]));
+
+        let actual = get_symbols(String::from(".....+.58."));
+        assert_eq!(actual, HashSet::from([5]));
     }
 }
