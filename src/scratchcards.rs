f7cadadcc8e227f64ab3298000757f1e9e1850ad
David Lucas <davidalucas92@gmail.com>
2024-01-26 10:13:41 -0500

    Adds the sum_total_winnings function
    Includes tests; part 1 complete

--- src/scratchcards.rs
+++ src/scratchcards.rs
@@ -1,12 +1,32 @@
-use std::collections::HashSet;
+use std::{
+    collections::HashSet,
+    fs::File,
+    io::{BufRead, BufReader, Result},
+};
+
+pub fn sum_total_winnings(path: &str) -> usize {
+    let mut sum = 0;
+
+    let file = File::open(path).unwrap();
+    for line in BufReader::new(file).lines() {
+        let line = line.unwrap();
+        let matches = count_matches(line.as_str());
+        if matches == 0 {
+            continue;
+        }
+        sum += 2usize.pow(matches as u32 - 1);
+    }
+
+    sum
+}
 
 fn count_matches(line: &str) -> usize {
     let split_card_title: Vec<&str> = line.split(": ").collect();
     let split_winners: Vec<&str> = split_card_title[1].split(" | ").collect();
 
-    let processed_winner_string = split_winners[0].replace("  ", " ");
+    let processed_winner_string = split_winners[0].trim().replace("  ", " ");
     let winners: HashSet<&str> = processed_winner_string.split(" ").collect();
-    let processed_actuals_string = split_winners[1].replace("  ", " ");
+    let processed_actuals_string = split_winners[1].trim().replace("  ", " ");
     let actuals: HashSet<&str> = processed_actuals_string.split(" ").collect();
 
     let intersection: HashSet<_> = winners.intersection(&actuals).collect();
